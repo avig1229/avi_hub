969b477e18bba4e1a086d3ff1369c1596c87f895
avig1229 <min-kuan@uni.minerva.edu>
2025-12-16 14:08:39 +0800

    fix project sequence

--- arts-portfolio/src/sanity/lib/queries.ts
+++ arts-portfolio/src/sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { groq } from 'next-sanity';
 
 // Get all projects for the grid
-export const PROJECTS_QUERY = groq`*[_type == "project"] | order(date desc) {
+export const PROJECTS_QUERY = groq`*[_type == "project"] | order(title == "shRma" desc, title == "Archive" asc, date desc) {
   _id,
   title,
   "slug": slug.current,
